139b98980f333064c570227312dc173f8aab9612
Carol Peña <anacarolpc11@gmail.com>
2022-06-14 11:52:58 -0500

    first commit

---
+++ hola.py
@@ -0,0 +1,10 @@
+def saludo():
+    name=input("¿Cuál es tu nombre?: ")
+    print("Hola "+name.upper())
+    age=input("¿Cuál es tu edad?: ")
+    age=int(age)
+    print("Tienes ",age," años y en 3 años tendras",(age+3))
+
+if __name__=='__main__':
+    #Linea para ejecutarse    
+    saludo()
