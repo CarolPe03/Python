7e039231957fdeaab1935d5011ff2a607f0f681d
Carol Peña <anacarolpc11@gmail.com>
2022-06-21 17:56:05 -0500

    7_3-Diccionarios

---
+++ diccionario.py
@@ -0,0 +1,17 @@
+def run():
+    mi_diccionario={
+        'llave1':1,
+        'llave2':2,
+        'llave3':3,
+    }
+
+    poblacion_paises={
+        'Argentina': 24244,
+        'Brasil': 34244,
+        'Colombia': 44244,
+    }
+
+    
+    
+    if __name__ == '__main__':
+        run()
